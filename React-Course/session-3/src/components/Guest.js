f206220024c91e34da19b611cde75e503fc4eddb
yuuukiDev <mohamedemadeldin34@gmail.com>
2024-01-31 10:36:02 +0200

    new update

--- React-Course/session-3/src/components/Guest.js
+++
@@ -1,9 +0,0 @@
-const Guest = () => {
-    return (
-        <>
-        <h1>Hello, Guest!</h1>
-        </>
-    )
-}
-
-export default Guest;
--- React-Course/session-3/src/components/Welcome.js
+++
@@ -1,8 +0,0 @@
-const Welcome = (props) => {
-    return (
-        <h1>Welcome {props.name} </h1>
-    );
-}
-
-
-export default Welcome;
